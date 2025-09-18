dd3b2c3abc59096490f2162933d969ce9db9f868
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2025-09-18 20:29:54 +0200

    a

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,27 +9,7 @@ struct ValueMeta {
 
 /// A WebSocket echo server
 fn main() {
-    let mut testi32: Vec<RwLock<i32>> = Vec::new();
-    let mut testi32Meta: Vec<RwLock<ValueMeta>> = Vec::new();
-
-    fn register_value(index: Option<usize>, _owner: i32) {
-        let index = match index {
-            Some(index) => index,
-            None => testi32.len(),
-        };
-        while testi32.len() <= index {
-            testi32.resize_with(100, || RwLock::new(0));
-        }
-        return index;
-    }
-
-    let mut get_value = |index: usize| {
-        return testi32[index].read().unwrap().clone();
-    };
-
-    register_value(None, 1);
-
-    println!("Hello, world! {:?}", testi32)
+    println!("Hello, world! {:?}", 55)
     // let server = TcpListener::bind("127.0.0.1:9001").unwrap();
 
     // for stream in server.incoming() {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,45 +1,7 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
-import { name, dependencies } from "./package.json";
-import dts from "vite-plugin-dts";
 
-export default defineConfig(({ command, mode }) => {
-  console.log(command, mode);
-
-  switch (command) {
-    case "serve":
-      return {
-        server: {
-          host: true,
-          port: 666,
-        },
-        build: {
-          outDir: "../docs",
-          emptyOutDir: true,
-        },
-        preview: {
-          port: 666,
-        },
-        root: "./src",
-      };
-    case "build":
-      if (mode === "tests" || mode === "production") {
-        return {
-          root: "./cypress/pages",
-          build: {
-            outDir: "./dist",
-          },
-          base: "",
-        };
-      } else {
-        return {
-          root: "./src",
-          build: {
-            outDir: "../docs",
-            emptyOutDir: true,
-          },
-          base: "",
-        };
-      }
-  }
+export default defineConfig({
+  server: {
+    port: 5151,
+  },
 });
