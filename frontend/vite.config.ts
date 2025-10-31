e2a722d5672ec4b2c9088f45da37addc465bc111
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2025-10-31 18:05:58 +0100

    a

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
   server: {
     port: 9999,
   },
